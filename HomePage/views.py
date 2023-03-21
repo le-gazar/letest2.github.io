8b1763703b9c4fbe84fb31481a9add9484477979
gazar <josuegazari@gmail.com>
2023-03-21 17:01:49 +0100

    j'ai modifier home en index

--- HomePage/views.py
+++ HomePage/views.py
@@ -3,7 +3,7 @@ from .models import Product
 # Create your views here.
 
 def home(request):
-    return render (request,'home.html')
+    return render (request,'index.html')
 
 
 def josue(request):
